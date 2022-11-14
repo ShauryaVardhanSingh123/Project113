4551a00445a809bfeb20e1ca1aca9380310723d4
ShauryaVardhanSingh123 <84715057+ShauryaVardhanSingh123@users.noreply.github.com>
2022-11-14 19:36:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function setup() {
+    canvas=createCanvas(600, 350);
+    canvas.center();
+    video=createCapture(VIDEO);
+    image(video,0,0,600,500);
+}
+
+function draw() {
+    circle(25,25,55);
+    circle(575,25,55);
+    circle(25,325,55);
+    circle(575,325,55);
+    rect(52.5,16,495,20);
+    rect(52.5,316,495,20);
+    rect(15,53,20,245);
+    rect(565,53,20,245);
+}
